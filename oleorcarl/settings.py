6552a65933169b60af08ff9842119216f1149523
Alistair Pattison <alistairpattison@gmail.com>
2022-10-05 22:08:50 -0500

    add settings file

---
+++ oleorcarl/settings.py
@@ -0,0 +1,8 @@
+DATABASE_PATH = "./db.sqlite"
+COOKIE_PATH = "./cookies.secret"
+
+CARLETON_DIRECTORY_URL = "https://www.carleton.edu/directory/"
+CARLETON_IMAGE_URL = "https://apps.carleton.edu/stock/ldapimage.php?id={}"
+
+OLAF_DIRECTORY_URL = "https://www.stolaf.edu/directory/search/"
+OLAF_IMG_URL = "https://www.stolaf.edu/stofaces/face.cfm?username={}&fullsize"
